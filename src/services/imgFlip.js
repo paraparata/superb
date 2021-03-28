cd1194840214bd9e6cd55950ca76645a89e7be97
paraparata <srizalmp@gmail.com>
2021-03-28 19:33:13 +0700

    first superb-next fly

--- src/services/imgFlip.js
+++
@@ -1,6 +0,0 @@
-async function imgFlip() {
-  const res = await fetch("https://api.imgflip.com/get_memes");
-  return res.json();
-}
-
-export { imgFlip };
--- src/styles/variables.js
+++
@@ -1,4 +0,0 @@
-const BREAKPOINTS = [640, 768, 1024, 1280, 1536];
-const mq = BREAKPOINTS.map((bp) => `@media (min-width: ${bp}px)`);
-
-export { BREAKPOINTS, mq };
